e686bcc7c01824f19e4af0fa80e548b8c7563200
tpenskaya <tayaserf@gmail.com>
2021-10-26 03:23:01 +0300

    Added final version of algo

--- demosaicing/src/vng_algo.py
+++ demosaicing/src/vng_algo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 directions = {
     'N': [-1, 0],
@@ -12,6 +13,13 @@ directions = {
 }
 
 
+def get_color_idx(rgb):
+    idx = -1
+    if rgb.any():
+        idx = np.nonzero(rgb)[0][0]
+    return idx
+
+
 def get_grad(m, direction):
     up, right = direction
     grad = abs(sum(m[2 + up][2 + right] - m[2 - up][2 - right])) + \
@@ -23,48 +31,87 @@ def get_grad(m, direction):
                 0.5 * abs(sum(m[2][2 + right] - m[2 - up][2]))
     else:
         directed_m = m[min(1, 2 + 2 * up): max(4, 2 + 2 * up + 1), min(1, 2 + 2 * right): max(4, 2 + 2 * right + 1)]
-        if directed_m.shape != (4, 3):
-            directed_m = directed_m.T
+        if directed_m.shape != (4, 3, 3):
+            directed_m = np.transpose(directed_m, (1, 0, 2))
         for i in range(2):
             grad += 0.5 * abs(sum(directed_m[i][0] - directed_m[i + 2][0])) + \
                     0.5 * abs(sum(directed_m[i][2] - directed_m[i + 2][2]))
     return grad
 
 
-def get_rgb(m, direction):
+def get_rgb(m, direction, center_idx):
     rgb = np.zeros(3)
     up, right = direction
 
-    idx_center = list(map(bool, m[2][2])).index(True)
-    rgb[idx_center] = 0.5 * sum(m[2][2] + m[2 + 2 * up][2 + 2 * right])
+    center_vec = m[2][2] + m[2 + 2 * up][2 + 2 * right]
+    rgb[center_idx] = 0.5 * sum(center_vec)
 
-    idx_next_to_center = list(map(bool, m[2 + up][2 + right])).index(True)
-    rgb[idx_next_to_center] = m[2 + up][2 + right]
+    next_idx = get_color_idx(m[2 + up][2 + right])
+    if next_idx != -1:
+        rgb[next_idx] = sum(m[2 + up][2 + right])
 
-    last_idx = next(x for x in range(3) if x != idx_center and x != idx_next_to_center)
-    directed_m = m[min(2, 2 + 2 * up): max(3, 2 + 2 * up + 1),
-                   min(2, 2 + 2 * right): max(3, 2 + 2 * right + 1)].flatten()
-    others = list(filter(lambda x: x[last_idx], directed_m))
-    rgb[last_idx] = sum(others) / len(others)
+    if up and right:
+        horizon_vec = m[2 + up][2] + m[2 + up][2 + 2 * right]
+        horizon_idx = get_color_idx(horizon_vec)
+        if horizon_idx != -1:
+            rgb[horizon_idx] += 0.5 * sum(horizon_vec)
 
+        vertical_vec = m[2][2 + right] + m[2 + 2 * up][2 + right]
+        vertical_idx = get_color_idx(vertical_vec)
+        if vertical_idx != -1:
+            rgb[vertical_idx] += 0.5 * sum(vertical_vec)
+
+        if center_idx != 1:
+            rgb[1] *= 0.5
+    else:
+        if center_idx == 1:
+            rb_others_vec = m[2 + right][2 + up] + m[2 + 2 * up + right][2 + up + 2 * right] + \
+                            m[2 - right][2 - up] + m[2 + 2 * up - right][2 - up + 2 * right]
+            rb_others_idx = get_color_idx(rb_others_vec)
+            if rb_others_idx != -1:
+                rgb[rb_others_idx] = 0.25 * sum(rb_others_vec)
+        else:
+            g_others_vec = m[2 + up + right][2 + up + right] + m[2 + up - right][2 - up + right]
+            g_others_idx = get_color_idx(g_others_vec)
+            if g_others_idx != -1:
+                rgb[g_others_idx] = 0.5 * sum(g_others_vec)
     return rgb
 
 
-def get_matrix_center(matrix):
+def get_center(m, center_idx):
+    matrix = np.copy(m)
     grads = {}
     for name in directions.keys():
         grads[name] = get_grad(matrix, directions[name])
     min_grad = min(grads.values())
     max_grad = max(grads.values())
     t = 1.5 * min_grad + 0.5 * (max_grad + min_grad)
-    under_t_grads = {k: v for k, v in grads.items() if v < t}
+    under_t_grads = {k: v for k, v in grads.items() if v <= t}
+
     rgb = np.zeros(3)
     for name in under_t_grads.keys():
-        rgb += get_rgb(matrix, directions[name])
+        rgb += get_rgb(matrix, directions[name], center_idx)
 
     center_rgb = matrix[2][2]
-    idx_center = list(map(bool, center_rgb)).index(True)
-    idx_others = list(x for x in range(3) if x != idx_center)
+
+    idx_others = list(x for x in range(3) if x != center_idx)
     for other_id in idx_others:
-        center_rgb[other_id] = center_rgb[idx_center] + (rgb[other_id] - rgb[idx_center]) / len(under_t_grads)
+        if under_t_grads:
+            center_rgb[other_id] = center_rgb[center_idx] + (rgb[other_id] - rgb[center_idx]) / len(under_t_grads)
     return center_rgb
+
+
+def demosaicing_image(img_matrix):
+    h, w, _ = img_matrix.shape
+    new_img_matrix = np.zeros((h, w, 3), dtype=np.uint8)
+    center_idx = get_color_idx(img_matrix[2][2])
+    next_idx = get_color_idx(img_matrix[2][3])
+    with tqdm(total=h * w, desc='VNG progress bar') as vng_progress_bar:
+        for i in range(h - 5):
+            cur_center = center_idx if not i % 2 else next_idx
+            cur_next = next_idx if not i % 2 else next(x for x in range(3) if x != center_idx and x != next_idx)
+            for j in range(w - 5):
+                cur_idx = cur_center if not j % 2 else cur_next
+                new_img_matrix[i + 2][j + 2] = get_center(img_matrix[i: i + 5, j: j + 5], cur_idx)
+                vng_progress_bar.update(1)
+    return new_img_matrix
